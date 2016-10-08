18609de4c833600c25eef2553a20eb1036df04b6
Seviciu Cosmin <zetcoby@gmail.com>
2016-10-08 14:51:34 +0300

    finished all the svg alements get lengths, elements suported : rect, circle, ellipse, polygon, polyline, line, path

--- src/zMotion-vanilla.js
+++ src/zMotion-vanilla.js
@@ -84,11 +84,13 @@
 
         /**
          * @description
-         * gets the length of a polygon
+         * gets the length of a polygon or a polyline
          * @param  {jQuery | HTML obj} node
+         * @param  {bool} isPolyLine ,
+         * if true calculates the length of a polyline
          * @return {Number}
          */
-        getPolygonLength : function (node) {
+        getPolygonLength : function (node, isPolyLine) {
             var n               = this.prepareNode(node);
             var points          = n.points;
             var pointsLength    = points.length;
@@ -107,7 +109,9 @@
                 for(i = 0 ; i < pointsLength - 1 ; i++) {
                     len += this.dist(points[i], points[i+1]);
                 }
-                len += this.dist(points[0], points[points.length-1]);
+                if (!isPolyLine) {
+                    len += this.dist(points[0], points[points.length-1]);
+                }
                 return len;
             }
         },
@@ -132,6 +136,56 @@
             }
 
             return this.dist(c1,c2)
+        },
+
+        /**
+         * @description
+         * gets the length of a rect node
+         * @param  {jQuery | HTML obj} node
+         * @return {Number}
+         */
+        getRectLength : function (node) {
+            var n = this.prepareNode(node);
+            var w = parseInt(n.getAttribute('width'));
+            var h = parseInt(n.getAttribute('height'));
+            return w * 2 + h * 2;
+        },
+
+        /**
+         * @description
+         * gets the length of a circle node
+         * @param  {jQuery | HTML obj} node
+         * @return {Number}
+         */
+        getCircleLength : function (node) {
+            var n = this.prepareNode(node);
+            var r = n.getAttribute('r');
+            return 2 * Math.PI * r;
+        },
+
+        /**
+         * @description
+         * gets the length of a ellipse node
+         * @param  {jQuery | HTML obj} node
+         * @return {Number}
+         */
+        getEllipseLength : function (node) {
+            var n = this.prepareNode(node);
+            var rx = parseInt(n.getAttribute('rx'));
+            var ry = parseInt(n.getAttribute('ry'));
+            var h = Math.pow((rx-ry), 2) / Math.pow((rx+ry), 2);
+            return (Math.PI * ( rx + ry )) * (1 + ( (3 * h) / ( 10 + Math.sqrt( 4 - (3 * h) )) ));
+        },
+
+        /**
+         * @description
+         * gets the length of a path node
+         * @param  {jQuery | HTML obj} node
+         * @return {Number}
+         */
+        getPathLength : function (node) {
+            var n = this.prepareNode(node);
+            return n.getTotalLength();
         }
     }
 
